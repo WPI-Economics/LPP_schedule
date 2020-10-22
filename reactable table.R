e86e85089f7478b2d4c4c392ef750c3616cd42cb
guyweir <guy.weir@wpieconomics.com>
2020-10-22 17:49:49 +0100

    date import

--- reactable table.R
+++ reactable table.R
@@ -4,7 +4,7 @@ library(tidyverse)
 library(googlesheets4)
 library(reactable)
 
-gsheet_LPPdates <- read_sheet("1rzjtZdAyclF-RFuqZB32Wo0wiQNIXAqX-NFx03rQzAo", sheet = "RAW", col_types = "ccciccccccccccccc")
+gsheet_LPPdates <- read_sheet("1rzjtZdAyclF-RFuqZB32Wo0wiQNIXAqX-NFx03rQzAo", sheet = "RAW", col_types = "ccciccccDDccccDcD")
 #"ccciccccDDccccDcD"
 
 gsheet_LPPdates2 <- gsheet_LPPdates %>%  select(Theme, `Sub-theme`, Index, Slug, Frequency, `Pulication month...17`, `LPP link`)
