63038a215aaad2375b02bb7405ad166e9d2fb10d
guyweir <guy.weir@wpieconomics.com>
2021-10-21 09:30:00 +0100

    add backlog category

--- LPP updates schedule table 2.R
+++ LPP updates schedule table 2.R
@@ -10,25 +10,30 @@ library(lubridate)
 
 update.df <- readRDS("LPP_schedule_data_2021-10-19.RDS") #WILL NEED TO EDIT DATE HERE
 
-update.df <- arrange(update.df,`New data publication date`)
-sortorder <- c("January-2021","February-2021","March-2021","April-2021","May-2021",
-               "June-2021","July-2021","August-2021","September-2021","October-2021","November-2021","December-2021",
-               
-               "January-2022","February-2022","March-2022","April-2022","May-2022",
-               "June-2022","July-2022","August-2022","September-2022","October-2022","November-2022","December-2022",
-               
-               "January-2023","February-2023","March-2023","April-2023","May-2023",
-               "June-2023","July-2023","August-2023","September-2023","October-2023","November-2023","December-2023",
-               
-               "January-2024","February-2024","March-2024","April-2024","May-2024",
-               "June-2024","July-2024","August-2024","September-2024","October-2024","November-2024","December-2024")
+#create backlop category for processing months in the past
+update.df$month.formatted <- as.Date(paste0("01-",update.df$Month), format = "%d-%B-%Y")
+update.df$Month[update.df$month.formatted %m+% months(1) < (Sys.Date())] <- "Backlog"
 
-update.df <- update.df %>% arrange(match(Month, sortorder)) #SORT FIRST TO MAKE SURE URL INDEX IS CORRECT ORDER
+
+update.df <- arrange(update.df,month.formatted)
+# sortorder <- c("January-2021","February-2021","March-2021","April-2021","May-2021",
+#                "June-2021","July-2021","August-2021","September-2021","October-2021","November-2021","December-2021",
+#                
+#                "January-2022","February-2022","March-2022","April-2022","May-2022",
+#                "June-2022","July-2022","August-2022","September-2022","October-2022","November-2022","December-2022",
+#                
+#                "January-2023","February-2023","March-2023","April-2023","May-2023",
+#                "June-2023","July-2023","August-2023","September-2023","October-2023","November-2023","December-2023",
+#                
+#                "January-2024","February-2024","March-2024","April-2024","May-2024",
+#                "June-2024","July-2024","August-2024","September-2024","October-2024","November-2024","December-2024")
+
+# update.df <- update.df %>% arrange(match(Month, sortorder)) #SORT FIRST TO MAKE SURE URL INDEX IS CORRECT ORDER
 
 
 #The table 
 update.df2 <- update.df
-table <- update.df %>% select(-`data link`) %>% 
+table <- update.df %>% select(-`data link`, -month.formatted) %>% 
   
   reactable(groupBy = "Month", 
             columns = list(
